903b071f2e29cfa0e3ac1a677bfa8794cb7daf95
Shitiz672 <73930608+Shitiz672@users.noreply.github.com>
2020-11-05 18:21:54 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,22 @@
 var request = require('request');
 var express = require('express');
 var router = express.Router();
+var fs = require('fs'); 
 
 /*
   ALL OF THE ROUTES IN THIS PAGE REQUIRE AN AUTHENTICATED USER
 */
+console.log("Users %d",8);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-  console.log(req.user)
+  //console.log(JSON.parse(req) +" HAHHAH")
+  
+  fs.appendFile('userInfo.txt', JSON.stringify(req.user)+"\r\n", function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+}); 
 
   res.render('users', {
     title: 'Users',
@@ -20,19 +27,21 @@ router.get('/', function(req, res, next) {
 /* GET the profile of the current authenticated user */
 router.get('/profile', function(req, res, next) {
   request.get(
-    `https://${ process.env.SUBDOMAIN }.onelogin.com/oidc/2/me`,   
+    `https://bia-dev.onelogin.com/oidc/2/me`,   
     {
     'auth': {
       'bearer': req.session.accessToken
     }
-  },function(err, respose, body){
+  },function(err, respose, body)
+  {
 
-    console.log('User Info')
-    console.log(body);
+    console.log('User FF')
+	
+    console.log(req);
 
     res.render('profile', {
       title: 'Profile',
-      user: JSON.parse(body)
+      user: JSON.parse(req)
     });
 
   });
