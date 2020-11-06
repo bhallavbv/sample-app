6ad5be32a414a5b3810b46024dda7b478cabacc1
Shitiz672 <73930608+Shitiz672@users.noreply.github.com>
2020-11-06 22:15:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ var passport = require('passport');
 var passportSaml = require('passport-saml');
 
 var app = express();
+var cookieSession = require('cookie-session');
+
+var index=require('./routes/index');
+var users=require('./routes/users');
 
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-const baseUri = `https://${ process.env.SUBDOMAIN }.onelogin.com/oidc/2`
 
 passport.serializeUser((user, done) => {
   done(null, user);
@@ -28,20 +28,25 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser((user, done) => {
   done(null, user);
 });
+//"https://bia-dev.onelogin.com/trust/saml2/http-post/sso/a801d15c-f46a-447c-b256-106de17782cd",
 //entryPoint: "https://bia-dev.onelogin.com/trust/saml2/http-post/sso/1a62b78f-9251-4b17-b732-a9baef373bd0",
 //https://bia-dev.onelogin.com/trust/saml2/http-post/sso/d1cfb2df-830c-4629-8654-7e804ae481a5
 // SAML strategy for passport -- Single IPD https://bia-dev.onelogin.com/trust/saml2/http-post/sso/
+//https://bia-dev.onelogin.com/trust/saml2/http-post/sso/a801d15c-f46a-447c-b256-106de17782cd
+//MIIDzzCCAregAwIBAgIUWTCE0/DisfVFFRsf3vez83ASnqkwDQYJKoZIhvcNAQEFBQAwQTEMMAoGA1UECgwDQklBMRUwEwYDVQQLDAxPbmVMb2dpbiBJZFAxGjAYBgNVBAMMEU9uZUxvZ2luIEFjY291bnQgMB4XDTIwMTEwMjIwMTcyNFoXDTI1MTEwMjIw MTcyNFowQTEMMAoGA1UECgwDQklBMRUwEwYDVQQLDAxPbmVMb2dpbiBJZFAxGjAY BgNVBAMMEU9uZUxvZ2luIEFjY291bnQgMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8A MIIBCgKCAQEA0FxbNkednHlSUxmDE6hjEkIkV9dCqD1PuI/qFzjL8y6C7/NawEooGoc7kL+nsrJmhL0ct4snppKQH/IIrkf0Q8sUhs+4sZZtzMr0+mUydFbDKTDRVPeC9qVhob7T3HpSIUNSL3b83vGpZ5ZgLbYapKzgx85Zy2YwGjDNOoREna7iFfqcjoA8PC9gKyP5frrhitJ57ryOcNWZRuNbLF/lIysA+TU41XL6+1Fs2Zr+CF2oNr7EqRl6O7ZmGOQcVul0t2juipg0QIVgtuWKNSWtiAkTOIXm7PpLVBKWkLSr02lRuiKpNitsezJlYoE0pQwRdP3AC9JPDKGw7mw9YbcxJwIDAQABo4G+MIG7MAwGA1UdEwEB/wQCMAAwHQYDVR0OBBYEFNs41hw7eUXeU9v6GLdjgg0NkqxHMHwGA1UdIwR1MHOAFNs41hw7eUXeU9v6GLdjgg0NkqxHoUWkQzBBMQwwCgYDVQQKDANCSUExFTATBgNVBAsMDE9uZUxvZ2luIElkUDEaMBgGA1UEAwwRT25lTG9naW4gQWNjb3VudCCCFFkwhNPw4rH1RRUbH973s/NwEp6pMA4GA1UdDwEB/wQEAwIHgDANBgkqhkiG9w0BAQUFAAOCAQEAY1ph+5kAHnk24GrJaYGX9X2FMmjjD1jGKFQHG+JpqVp6O7L2RaZPM0Qg8Qla/mqCIwNINEingTE2IUxqOgwQMKCOfp09cW9TUw2kecjQAXg0sf7bPnlgajluIwYEvmby4rF3Se1eEpbZaGfeMETx5dC0UCZ2hkSoJ7ioMPFsl7+EFWND/3Juw1F1V04hR3ZmpzgTpcoT6rI770Lu4i5DQLAP4I/XBPNvJLDLHC9L5AFBVkONr1Z08zSNPLagIR33QTOCu/K7zo+pInp1v8KNEhwz+ucC9lg3tRHlZ0qLF6FH5QD3IbmjD2Q3Pcv2ZqUDU3okEqn+RkITnixddeYqnQ=="
+
 const strategy = new passportSaml.Strategy(
   {
-	entryPoint: "https://bia-dev.onelogin.com/trust/saml2/http-post/sso/1a62b78f-9251-4b17-b732-a9baef373bd0",
+      //https://login.salesforce.com?so=00D2y000000uBiN
+	entryPoint: "https://bia2.my.salesforce.com/idp/endpoint/HttpRedirect",
     issuer: "passport-saml",
-    path:"/login/callback",
-    cert: "MIIDzzCCAregAwIBAgIUWTCE0/DisfVFFRsf3vez83ASnqkwDQYJKoZIhvcNAQEFBQAwQTEMMAoGA1UECgwDQklBMRUwEwYDVQQLDAxPbmVMb2dpbiBJZFAxGjAYBgNVBAMMEU9uZUxvZ2luIEFjY291bnQgMB4XDTIwMTEwMjIwMTcyNFoXDTI1MTEwMjIw MTcyNFowQTEMMAoGA1UECgwDQklBMRUwEwYDVQQLDAxPbmVMb2dpbiBJZFAxGjAY BgNVBAMMEU9uZUxvZ2luIEFjY291bnQgMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8A MIIBCgKCAQEA0FxbNkednHlSUxmDE6hjEkIkV9dCqD1PuI/qFzjL8y6C7/NawEooGoc7kL+nsrJmhL0ct4snppKQH/IIrkf0Q8sUhs+4sZZtzMr0+mUydFbDKTDRVPeC9qVhob7T3HpSIUNSL3b83vGpZ5ZgLbYapKzgx85Zy2YwGjDNOoREna7iFfqcjoA8PC9gKyP5frrhitJ57ryOcNWZRuNbLF/lIysA+TU41XL6+1Fs2Zr+CF2oNr7EqRl6O7ZmGOQcVul0t2juipg0QIVgtuWKNSWtiAkTOIXm7PpLVBKWkLSr02lRuiKpNitsezJlYoE0pQwRdP3AC9JPDKGw7mw9YbcxJwIDAQABo4G+MIG7MAwGA1UdEwEB/wQCMAAwHQYDVR0OBBYEFNs41hw7eUXeU9v6GLdjgg0NkqxHMHwGA1UdIwR1MHOAFNs41hw7eUXeU9v6GLdjgg0NkqxHoUWkQzBBMQwwCgYDVQQKDANCSUExFTATBgNVBAsMDE9uZUxvZ2luIElkUDEaMBgGA1UEAwwRT25lTG9naW4gQWNjb3VudCCCFFkwhNPw4rH1RRUbH973s/NwEp6pMA4GA1UdDwEB/wQEAwIHgDANBgkqhkiG9w0BAQUFAAOCAQEAY1ph+5kAHnk24GrJaYGX9X2FMmjjD1jGKFQHG+JpqVp6O7L2RaZPM0Qg8Qla/mqCIwNINEingTE2IUxqOgwQMKCOfp09cW9TUw2kecjQAXg0sf7bPnlgajluIwYEvmby4rF3Se1eEpbZaGfeMETx5dC0UCZ2hkSoJ7ioMPFsl7+EFWND/3Juw1F1V04hR3ZmpzgTpcoT6rI770Lu4i5DQLAP4I/XBPNvJLDLHC9L5AFBVkONr1Z08zSNPLagIR33QTOCu/K7zo+pInp1v8KNEhwz+ucC9lg3tRHlZ0qLF6FH5QD3IbmjD2Q3Pcv2ZqUDU3okEqn+RkITnixddeYqnQ=="
-	},
+    path:"http://upgrade-project.herokuapp.com/login/callback",
+    cert: "MIIErDCCA5SgAwIBAgIOAXWcUJiBAAAAABDWMyswDQYJKoZIhvcNAQELBQAwgZAxKDAmBgNVBAMMH1NlbGZTaWduZWRDZXJ0XzA2Tm92MjAyMF8wNjQ4NDgxGDAWBgNVBAsMDzAwRDJ5MDAwMDAwdUJpTjEXMBUGA1UECgwOU2FsZXNmb3JjZS5jb20xFjAUBgNVBAcMDVNhbiBGcmFuY2lzY28xCzAJBgNVBAgMAkNBMQwwCgYDVQQGEwNVU0EwHhcNMjAxMTA2MDY0ODQ5WhcNMjExMTA2MDAwMDAwWjCBkDEoMCYGA1UEAwwfU2VsZlNpZ25lZENlcnRfMDZOb3YyMDIwXzA2NDg0ODEYMBYGA1UECwwPMDBEMnkwMDAwMDB1QmlOMRcwFQYDVQQKDA5TYWxlc2ZvcmNlLmNvbTEWMBQGA1UEBwwNU2FuIEZyYW5jaXNjbzELMAkGA1UECAwCQ0ExDDAKBgNVBAYTA1VTQTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALgsw7ggO36KdPUk/5uv/c5R8QS5XJ3tKRRmk5leoOJSsV6VvSdXEVj3NuF3NLvG4dS9dOpGLJ2dDG+MtxOMeM8D8wTRctE8AAD8dDukRIFp1XAiSjwNqOx8dJMEuaFu2pZRjoyLKBd68zK/o5z3wRcywHJl9kzYBgI3orQbP/u5iKbedv+Ur6C5+eJffMF1z99wis92LXJp3a6jePd6KmMJOia/tz1NxDgif8Q1CFWsMVfl1RMVNUKZlRbSUAchZFAO4C7+GGwPv6fnYvBrx410ZaIRaPJhLZHf/FzEfRkpTIMEEPfVg0d3i4M0hEb8VYBZrxA7d/g4rv7hvjo5XIcCAwEAAaOCAQAwgf0wHQYDVR0OBBYEFH7Baefd6i+vfZaQNOqrnJQx9zvhMA8GA1UdEwEB/wQFMAMBAf8wgcoGA1UdIwSBwjCBv4AUfsFp593qL699lpA06quclDH3O+GhgZakgZMwgZAxKDAmBgNVBAMMH1NlbGZTaWduZWRDZXJ0XzA2Tm92MjAyMF8wNjQ4NDgxGDAWBgNVBAsMDzAwRDJ5MDAwMDAwdUJpTjEXMBUGA1UECgwOU2FsZXNmb3JjZS5jb20xFjAUBgNVBAcMDVNhbiBGcmFuY2lzY28xCzAJBgNVBAgMAkNBMQwwCgYDVQQGEwNVU0GCDgF1nFCYgQAAAAAQ1jMrMA0GCSqGSIb3DQEBCwUAA4IBAQBWeSPqojAFWVnQdTGyfGz3bcXjy65/veVom3BZHTFfCgfr4f8+ayBIMBVXjRqWbg8SPqa6CQFU59lXr2aG+UEcdC9CBQqOEI6m2qnlou9vvxx9MeQjPPBhEAt8hVaTH7GTIGv9A7Axku3MxKomkSbrvUb92sUoR+nlvgDEeYPI684eRPWwCeHMdLs8DqzXbD0eqxY3T+5Y2gLixLQLGRlLR2tedIFMipcydn9SShIVyZYiRRvqdZHIfPDN/IaMKTVgfVbSvT6Jx1uqCXY28YGCHe0Kt1BN+9KRLd/2arKmolyf4VcNG1cLTQXPDC0hXtmQGnPq0ncqHgBQiuXdzCG2"    },
   (profile, done) => done(null, profile),
 );
 
 passport.use(strategy);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -53,14 +58,20 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); 
 
 // Passport requires session to persist the authentication
 // so were using express-session for this example
-app.use(session({
+app.use(cookieSession({
+  name:'ss',
   secret: 'secret squirrel',
-  resave: false,
-  saveUninitialized: true
+  cookie:{
+	  maxAge: 1000*60*60*24*7 // 1 week
+  },
+   
+  resave: true,    //false
+  saveUninitialized: true,
+  cookie:{secure:false}
 }))
 
 // Initialize Passport
@@ -69,16 +80,25 @@ app.use(passport.session());
 
 // Middleware for checking if a user has been authenticated
 // via Passport and OneLogin OpenId Connect
-function checkAuthentication(req,res,next){
-  if(req.isAuthenticated()){
+function checkAuthentication(req,res,next)
+{
+	//console.log(req);
+	//console.log(JSON.stringify(res));
+    //console.log(JSON.stringify(next));
+  if(req.isAuthenticated())
+  {
+	  console.log("Authenticated");
       next();
-  } else{
+  } 
+  else
+  {	  
       res.redirect("/");
+	  console.log("Not Authenticated");
   }
 }
 
 app.use('/', index);
-// Only allow authenticated users to access the /users route
+
 app.use('/users', checkAuthentication, users);
 
 // Initiates an authentication request with OneLogin
@@ -148,16 +168,7 @@ router.get('/login/sso',
  * If every thing validated we validates if user email present into user DB.
  * Then creates a session for the user set in cookies and do a redirect to Application
  */
-/*router.post('/login/sso/',
-  userAgentHandler,
-  passport
-    .authenticate('saml', { failureRedirect: '/', failureFlash: true }), (req, res, next) => {
-    const xmlResponse = req.body.SAMLResponse;
-    const parser = new Saml2js(xmlResponse);
-    req.samlUserObject = parser.toObject();
-    next();
-  },
-  (req, res) => userLogin.createUserSession(res, req)); */
+
   
   app.get('/login', passport.authenticate('saml', {
   successReturnToOrRedirect: "/"
